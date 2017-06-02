cd189a545acf73e2e77d3f85dfe2bb8c8637c31b
Alex Dillhoff <ajdillhoff@gmail.com>
2017-06-02 15:51:05 -0500

    Added visualization demo to show annotations on frames.

---
+++ demos/visualize_annotation.m
@@ -0,0 +1,39 @@
+% Author: Alex Dillhoff
+% Date: June 2, 2017
+% 
+% This demo shows how to visualize the hand location annotations on a frame.
+
+annotationDir = '/Users/alexdillhoff/Documents/Data Sets/1113_annotations/';
+dataDir = '/Volumes/ASLLVD/vid/';
+signers = {'tb1113', 'lb1113'};
+
+% Load the video and annotations
+signLoader = SignLoader(dataDir, annotationDir, signers);
+[vid, meta, annotations] = signLoader.loadSign(500);
+
+% Pick a frame
+frameIdx = 1;
+vidFrameIdx = frameIdx + meta.StartFrame - 1;
+frame = vidReadMex(vid, vidFrameIdx);
+
+% Display the frame
+imshow(uint8(frame));
+
+% Generate the boxes for annotations and display
+hand1Annotation = annotations{1}(frameIdx, :);
+bw = hand1Annotation(3);
+bh = hand1Annotation(4);
+bx = hand1Annotation(1);
+by = hand1Annotation(2);
+hand1Box = [bx, by, bw, bh];
+rectangle('Position', hand1Box, 'EdgeColor', 'red');
+
+if annotations{2} ~= []
+    hand2Annotation = annotations{2}(frameIdx, :);
+    bw = hand1Annotation(3);
+    bh = hand1Annotation(4);
+    bx = hand1Annotation(1);
+    by = hand1Annotation(2);
+    hand2Box = [bx, by, bw, bh];
+    rectangle('Position', hand2Box, 'EdgeColor', 'red');
+end
